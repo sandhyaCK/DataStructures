9d2cfd69fae583dc5677032183e5d9ee085ca8b2
sandhyaCK <csandhyait@gmail.com>
2019-12-23 13:10:09 +0530

    [Sandhya]modified

--- UnorderedLinkedlist.java
+++ UnorderedLinkedlist.java
@@ -5,31 +5,57 @@
    #***************************************************************************************************************/
 package com.Bridgelabz.DataStructures;
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.*;
 public class UnorderedLinkedlist {
 
+	Node head;
+	 public static class Node
+	{
+	String data;
+		Node next;
+		Node(String i){
+			this.data = i;
+			next = null;
+		}
+		}
+		 void display() {
+			Node n = head;
+			while(n!=null) {
+				System.out.println(n.data + " ");
+				n=n.next;
+			}
+		}
 	public static void main(String[] args) throws IOException {
-		
-		
-		LinkedList<String> l=new LinkedList<>();
-		File file = new File("/home/user/Desktop/files.txt");
-		
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		String st;
-		while((st=br.readLine())!=null) {
-			
-			
-		
-		l.add(st);
-		}
-		System.out.println(l);
-		
-		if(l.contains("ccc")) {
-			l.remove("cccc");
-		}
-		else {
-	 l.add("ssss");
-		}
-		System.out.println(l);
-		
+
+File file = new File("/home/user/Desktop/files.txt");
+
+UnorderedLinkedlist list=new UnorderedLinkedlist();
+BufferedReader br = new BufferedReader(new FileReader(file));
+String st[] = new String[100];
+while((br.readLine())!=null) {
+for(int i=0;i<file.length();i++) {
+	st[i] = br.readLine();
 }}
+list.head=new Node(st[0]);
+Node second=new Node(st[1]);
+Node third = new Node(st[2]);
+Node fourth=new Node(st[3]);
+Node aa = new Node(st[4]);
+list.head.next = second;
+second.next=third;
+third.next=fourth;
+list.display();
+/*if(list.equals("SHASHI")) {
+	Node bb = new Node("MANJU");
+	third.next = bb;
+	list.display();
+
+	
+}*.*/
+}
+
+	}
